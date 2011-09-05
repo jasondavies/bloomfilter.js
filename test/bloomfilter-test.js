287b266bab7025850f97b016518ec9e37ff08c11
Jason Davies <jason@jasondavies.com>
2011-09-05 13:48:44 +0100

    Add another test.

--- test/bloomfilter-test.js
+++ test/bloomfilter-test.js
@@ -7,13 +7,23 @@ var suite = vows.describe("bloomfilter");
 
 suite.addBatch({
   "bloom filter": {
-    "basic": function(bf) {
+    "basic": function() {
       var f = new BloomFilter(1000, 4),
           n1 = "Bess",
           n2 = "Jane";
       f.add(n1);
       assert.equal(f.test(n1), true);
       assert.equal(f.test(n2), false);
+    },
+    "basic uint32": function() {
+      var f = new BloomFilter(1000, 4),
+          n1 = "\u100",
+          n2 = "\u101",
+          n3 = "\u103";
+      f.add(n1);
+      assert.equal(f.test(n1), true);
+      assert.equal(f.test(n2), false);
+      assert.equal(f.test(n3), false);
     }
   }
 });
