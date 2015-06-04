27a37d87c2e168770e7709329a099ab13354764d
Jason Davies <jason@jasondavies.com>
2015-06-04 18:14:44 +0100

    Simplify.

--- bloomfilter.js
+++ bloomfilter.js
@@ -90,40 +90,28 @@
 
   // Fowler/Noll/Vo hashing.
   function fnv_1a(v) {
-    var n = v.length,
-        a = 2166136261,
-        c,
-        d,
-        i = -1;
-    while (++i < n) {
-      c = v.charCodeAt(i);
-      if (d = c & 0xff000000) {
-        a ^= d >> 24;
-        a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
-      }
-      if (d = c & 0xff0000) {
-        a ^= d >> 16;
-        a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
-      }
-      if (d = c & 0xff00) {
+    var a = 2166136261;
+    for (var i = 0, n = v.length; i < n; ++i) {
+      var c = v.charCodeAt(i),
+          d = c & 0xff00;
+      if (d) {
         a ^= d >> 8;
         a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
       }
       a ^= c & 0xff;
+      // a = a * 16777619 mod 2**32
       a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
     }
-    // From http://home.comcast.net/~bretm/hash/6.html
-    a += a << 13;
-    a ^= a >> 7;
-    a += a << 3;
-    a ^= a >> 17;
-    a += a << 5;
-    return a & 0xffffffff;
+    return fnv_mix(a);
   }
 
   // One additional iteration of FNV, given a hash.
   function fnv_1a_b(a) {
-    a += (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
+    return fnv_mix(a + (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24));
+  }
+
+  // See https://web.archive.org/web/20131019013225/http://home.comcast.net/~bretm/hash/6.html
+  function fnv_mix(a) {
     a += a << 13;
     a ^= a >> 7;
     a += a << 3;
